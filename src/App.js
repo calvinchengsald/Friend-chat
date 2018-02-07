d80e5513fcb95f52c9091c7e1a3da3e1de6fc1ac
Calvin <calvin.cheng@stonybrook.edu>
2018-02-06 22:22:32 -0500

    added bootstrap, need to pull to bottom

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import {Route, Link} from 'react-router-dom';
 import Home from './scene/Home.js'
+import * as firebase from 'firebase';
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDU9b2q3rpSwM9x39p7mw40mTxgrbZhp4Q",
+    authDomain: "bloc-chat-9b080.firebaseapp.com",
+    databaseURL: "https://bloc-chat-9b080.firebaseio.com",
+    projectId: "bloc-chat-9b080",
+    storageBucket: "bloc-chat-9b080.appspot.com",
+    messagingSenderId: "148256012013"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <nav>
-            <Link to='/'>Home</Link>
-          </nav>
-        </header>
-        <main>
-          <Route exact path='/' component={Home}/>
-        </main>
+        <div className="container fill">
+          <div className="row">
+            <header className="App-header col-lg-12">
+              <nav>
+                <Link to='/'><input type="button" className="btn-lg" value="Home"></input></Link>
+              </nav>
+            </header>
+          </div>
+          <main>
+            <Route exact path='/' component={Home}/>
+          </main>
+        </div>
       </div>
     );
   }
--- src/scene/Home.js
+++ src/scene/Home.js
@@ -4,9 +4,14 @@ class Home extends Component{
 
   render(){
     return(
-      <div>
-        Home page will be here
-      </div>
+        <div className ='row'>
+          <div className='left-container col-lg-3'>
+            <h1>This is the left side adsfakdsfjadsklfjldsalkfsdk</h1>
+          </div>
+          <div className='right-container col-lg-9'>
+            <p>This is the right side</p>
+          </div>
+        </div>
     );
   }
 }
